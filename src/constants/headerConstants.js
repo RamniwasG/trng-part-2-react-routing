02bd085fab87d8cfc44b03a9df70e02204796ecc
Ramniwas Gupta <ramniwas.nitrr15@gmail.com>
2025-03-01 12:23:27 +0530

    gallery menu removed

--- src/constants/headerConstants.js
+++ src/constants/headerConstants.js
@@ -1,7 +1,7 @@
 export const headerMenuList = [
     { label: 'Home', url: '/home' },
     { label: 'Products', url:'/products' },
-    { label: 'Gallery', url:'/gallery' },
+    // { label: 'Gallery', url:'/gallery' },
     { label: 'Services' , url: '/services' },
     { label: 'About Us', url:'/about' }
 ];
